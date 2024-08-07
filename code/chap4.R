159e24e274167e7ffe1380029a0594e6fccb594a
BK <bklee@nyu.edu>
2024-08-06 23:09:05 -0400

    update files

--- code/chap4.R
+++ code/chap4.R
@@ -96,31 +96,6 @@ g_tree <- make_tree(10, 3, mode = "undirected")
 plot(g_tree, vertex.shape = 'none', vertex.label.cex = 0.1,
      edge.width=0.1, layout = layout.reingold.tilford(g_tree, root=1))
 
-## 흐름을 강조하는 시각화 방법들 (@@ this is not included in the text book?)
-## 생키 다이어그램 https://r-graph-gallery.com/323-sankey-diagram-with-the-networkd3-library.html
-#install.packages('networkD3')
-
-# Load package
-library(networkD3)
- 
-# Load energy projection data
-URL <- "https://cdn.rawgit.com/christophergandrud/networkD3/master/JSONdata/energy.json"
-Energy <- jsonlite::fromJSON(URL)
-
-# Now we have 2 data frames: a 'links' data frame with 3 columns (from, to, value), and a 'nodes' data frame that gives the name of each node.
-head(Energy$links)
-head(Energy$nodes)
- 
-# Thus we can plot it
-p <- sankeyNetwork(Links = Energy$links, Nodes = Energy$nodes, Source = "source",
-              Target = "target", Value = "value", NodeID = "name",
-              units = "TWh", fontSize = 12, nodeWidth = 30)
-p
-
-# save the widget
-library(htmlwidgets)
-saveWidget(p, file=paste0( getwd(), "/HtmlWidget/sankeyEnergy.html"))
-
 ## 무작위 배열
 plot(g_er, layout = layout_randomly(g_er), 
      vertex.label.cex = 1, edge.arrow.size = 0.5)
